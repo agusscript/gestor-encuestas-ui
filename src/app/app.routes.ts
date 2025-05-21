ca7b23a25ccdf34d5647d5c1f0225fadd981f982
agusscript <agus.sanchez.240@gmail.com>
2025-05-21 18:25:41 -0300

    Add routing for survey features

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CreateSurveyComponent } from './components/survey-create/survey-create.component';
+import { SurveyAnswerComponent } from './components/survey-answer/survey-answer.component';
+import { SurveySuccessComponent } from './components/survey-success/survey-success.component';
+import { SurveyResultComponent } from './components/survey-result/survey-results.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'survey/create',
+    component: CreateSurveyComponent,
+  },
+  {
+    path: 'survey/answer/:id/:participationId',
+    component: SurveyAnswerComponent,
+  },
+  {
+    path: 'survey/success/:id/:participationId/:visualizationId',
+    component: SurveySuccessComponent,
+  },
+  {
+    path: 'survey/result/:id/:visualizationId',
+    component: SurveyResultComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
