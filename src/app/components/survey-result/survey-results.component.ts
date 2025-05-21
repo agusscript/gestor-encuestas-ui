8543dd2bbc7b257ade920672bc3d2941ad680438
agusscript <agus.sanchez.240@gmail.com>
2025-05-21 18:23:48 -0300

    Add survey-result component

---
+++ src/app/components/survey-result/survey-results.component.ts
@@ -0,0 +1,83 @@
+import { SurveyVisualizationResponseDto } from '../../dtos/response/survey-visualization-response.dto';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SurveyService } from '../../services/survey.service';
+import { ActivatedRoute } from '@angular/router';
+import { MessageService } from 'primeng/api';
+
+@Component({
+  selector: 'app-survey-result',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './survey-result.component.html',
+  styleUrl: './survey-result.component.css',
+})
+export class SurveyResultComponent implements OnInit {
+  survey: SurveyVisualizationResponseDto | null = null;
+  groupedAnswers: any[] = [];
+  expandedCards: boolean[] = [];
+
+  surveyService = inject(SurveyService);
+  messageService = inject(MessageService);
+  route = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+    const visualizationId = this.route.snapshot.paramMap.get('visualizationId');
+
+    if (visualizationId) {
+      this.surveyService.findOneByVisualizationId(visualizationId).subscribe({
+        next: (data) => {
+          this.survey = data;
+          this.groupAnswersByIndex(data.questions);
+        },
+        error: () => (this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'Error al cargar los resultados'
+        }))
+      });
+    } else {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'ID de visualización inválido'
+      });
+    }
+
+    if (this.groupedAnswers) {
+      this.expandedCards = new Array(this.groupedAnswers.length).fill(false);
+    }
+  }
+
+  toggleCard(index: number) {
+    this.expandedCards[index] = !this.expandedCards[index];
+  }
+
+  private groupAnswersByIndex(questions: any[]) {
+    const responseCount = questions[0]?.answers.length || 0;
+    const grouped: any[] = [];
+
+    for (let i = 0; i < responseCount; i++) {
+      const answers = questions.map((q) => {
+        const answer = q.answers[i];
+        return {
+          question: q.text,
+          type: q.type,
+          response: answer.text,
+          selectedOptions: answer.selectedOptions
+        };
+      });
+
+      grouped.push({
+        label: `Respuesta #${i + 1}`,
+        answers
+      });
+    }
+
+    this.groupedAnswers = grouped;
+  }
+
+  get totalResponses(): number {
+    return this.groupedAnswers.length;
+  }
+}
