7fa816b48dcfcb01272d8e9c6d463dfc4c5acf45
agusscript <agus.sanchez.240@gmail.com>
2025-05-21 09:49:05 -0300

    Update styles for home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { ImageModule } from 'primeng/image';
 
 @Component({
   selector: 'app-home',
-  imports: [ButtonModule, RouterModule],
+  imports: [ButtonModule, RouterModule, ImageModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
